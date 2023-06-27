3794c4396f8c02e3b4584d36c08b5bbbc0c0d7d5
subit419 <12897494+subit419@users.noreply.github.com>
2023-06-27 15:45:36 -0400

    completed temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let convertedTemp = ((farenheit - 32) * (5/9));
+  let roundedResult = Math.round(convertedTemp * 10) / 10;
+
+
+  return roundedResult;   
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(celsius) {
+  let convertedTemp = (celsius * (9/5) + 32 );
+  let roundedResult = Math.round(convertedTemp * 10) / 10;
+  return roundedResult;
 };
 
 // Do not edit below this line
