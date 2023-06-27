822559e6d50749f4317dff8a095287be3bc7a7fc
subit419 <12897494+subit419@users.noreply.github.com>
2023-06-27 15:04:51 -0400

    Leap years check logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // check if it is divisible by 4
+    // if it is also divisible by 100 then it is not a leap year 
+    // unless it is divisible by 400 then it is.
+    if (year % 4 == 0) {
+        if ((year % 100 == 0) && (year % 400 == 0)) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
