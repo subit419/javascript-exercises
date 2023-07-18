98338b49c748096f03510342d5416f5d6030a3f1
subit419 <12897494+subit419@users.noreply.github.com>
2023-07-18 12:44:43 -0400

    calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbersToSum) {
+	return numbersToSum.reduce((partialSum, a) => partialSum + a, 0);
 };
 
 const multiply = function() {
+  var subTotal = 1;
+  var args = Array.prototype.slice.call(arguments);
+  args.forEach(arg => {
+    subTotal *= arg;
+  });
 
+  return subTotal;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let answer = 1;
+	if (n == 0 || n == 1){
+    return answer;
+  } else {
+    for (let i = n; i >= 1; i--) {
+      answer *= i;
+    }
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
