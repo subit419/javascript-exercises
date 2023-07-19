9adc4a1d26d673e4ad7d5a276a2d4aa9173d5530
subit419 <12897494+subit419@users.noreply.github.com>
2023-07-19 11:44:41 -0400

    palindromes exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (stringToCheck) {
+    var cleanedString = stringToCheck.replace(/[^\w\s\']|_/g, "")
+                                     .replace(/\s+/g, "");
 
+    var fullPhrase = [...cleanedString];
+    
+    for (let index = 0; index < fullPhrase.length / 2; index++) {
+        var currentFrontLetter = fullPhrase[index];
+        var currentBackLetter = fullPhrase[fullPhrase.length - 1 - index];
+
+        if (currentFrontLetter.toUpperCase() != currentBackLetter.toUpperCase()){
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
