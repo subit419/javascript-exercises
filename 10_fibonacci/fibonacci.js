89e7047c5e36024230d0115a09943b20f7fffaa3
subit419 <12897494+subit419@users.noreply.github.com>
2023-08-15 13:36:55 -0400

    fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let seqNumberOne = 0;
+    let seqNumberTwo = 1;
+    let seqNumberThree = 1;
 
+    if (n == 0) {
+        return seqNumberOne;
+    } else if (n < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i <= n; i++) {
+        seqNumberThree = seqNumberOne + seqNumberTwo;
+        seqNumberOne = seqNumberTwo;
+        seqNumberTwo = seqNumberThree;
+    }
+    return seqNumberTwo;
 };
 
 // Do not edit below this line
